b36b0541c08def56e28fb0d42f33be490d9afef2
NaveeraRajChuhan <nnfstars@gmail.com>
2025-03-20 20:37:39 +0200

    else if2

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-var age = +prompt('enter your age')
-if (age <=100 && age >= 0   ){
-    if (age >=90)
-        document.write("you are too much old but if you want license so we will give you")
-} elseif(age >=90){
-document.write("you are too much old but if you want license so we will give you")
+var age = +prompt("enter your age");
+if (age <= 100 && age >= 0) {
+  if (age >= 80)
+    document.write(
+      "you are too much old but if you want license so we will give you"
+    );
+  else if (age >= 70) {
+    document.write(
+      "you are too  old but if you want license so we will give you"
+    );
+  } else if (age >= 18) {
+    document.write("you can get a driving license");
+  } else {
+    document.write("you can get a driving license");
+  }
+} else {
+    
 }
-
-
